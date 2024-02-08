37f944624f5c3fba0e32c5245bdc46687950c17f
Artur Borkowski <artbor2@gmail.com>
2024-02-08 11:43:10 +0100

    Wartośvi całkowite

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int myAge = 33;
+
+int myAge = int.MaxValue;
+Console.WriteLine(myAge);
+myAge = int.MinValue;
+Console.WriteLine(myAge);
+myAge = 120;
+int newAge = myAge + 5;
+Console.WriteLine(newAge);
+
+uint myVar2 = uint.MaxValue;
+Console.WriteLine(myVar2);
+myVar2 = uint.MinValue;
+Console.WriteLine(myVar2);
+
+long myVar3 = long.MaxValue;
+Console.WriteLine(myVar3);
+myVar3 = long.MinValue; 
+Console.WriteLine(myVar3);
+
+ulong myVar4 = long.MaxValue;
+Console.WriteLine(myVar4);
+myVar4 = ulong.MinValue;
+Console.WriteLine(myVar4);
