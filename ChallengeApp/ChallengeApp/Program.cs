35b40a76b50b771d43ff33d292c62e79168b2b92
Artur Borkowski <artbor2@gmail.com>
2024-02-11 10:55:41 +0100

    Dzień 5 - zadanie domowe - rozwiazanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,57 @@
-﻿/// Przygotuj program, który policzy ile jakich cyfr
-/// występuje w podanej liczbie:
-///  
-/// Przykład:
-/// Wyniki dla liczby: 4566
-/// 0 => 0
-/// 1 => 0
-/// 2 => 0
-/// 3 => 0
-/// 4 => 1
-/// 5 => 1
-/// 6 => 2
-/// 7 => 0
-/// 8 => 0
-/// 9 => 0
-/// 
-int number = 4566;
+﻿int number = 4566;
 string numberInString = number.ToString();
 char[] letters = numberInString.ToCharArray();
+
+int[] digits = new int[10]; // cyfry 0 - 9 
+
+foreach(char c in letters)
+{
+    if (c == '0')
+    {
+        digits[0]++;
+    }
+    else if (c == '1')
+    {
+        digits[1]++;
+    }
+    else if (c == '2')
+    {
+        digits[2]++;
+    }
+    else if (c == '3')
+    {
+        digits[3]++;
+    }
+    else if (c == '4')
+    {
+        digits[4]++;
+    }
+    else if (c == '5')
+    {
+        digits[5]++;
+    }
+    else if (c == '6')
+    {
+        digits[6]++;
+    }
+    else if (c == '7')
+    {
+        digits[7]++;
+    }
+    else if (c == '8')
+    {
+        digits[1]++;
+    }
+    else if (c == '9')
+    {
+        digits[9]++;
+    }
+}
+
+Console.WriteLine("Number: " + number);
+for (int i = 0; i < digits.Length; i++)
+{
+    Console.WriteLine(i + " => " + digits[i]);
+}
+Console.WriteLine();
+ 
