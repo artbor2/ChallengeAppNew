c5628d7469168e444ec2f826240010aaaf878896
Artur Borkowski <artbor2@gmail.com>
2024-02-11 09:32:21 +0100

    Dzień 4 - zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,18 @@
-﻿/// Napisz w Program.cs kod, w którym:
-/// - zadeklarujesz zmienną z imieniem
-/// - zadeklarujesz zmienną z płcią (dobierz odpowiednią)
-/// - zadeklarujesz zmienną z wiekiem
-/// 
-/// A następnie napisz kilka instrukcji warunkowych,
-/// które przeanalizują te dane w następujący sposób:
-///  
-/// (1): jeśli mamy do czynienia z kobietą i ma poniżej 30 lat, to zwróć komunikat:
-///      "Kobieta poniżej 30 lat"
-/// (2): jeśli mamy do czynienia z osobą o imieniu Ewa i ma ona równe 30 lat,
-///      to zwróć komunikat: "Ewa, lat 30"
-/// (3): jeśli mamy do czynienia z mężczyzną i ma poniżej 18 lat zwróć komunikat:
-///      "Niepełnoletni mężczyzna"
-/// (4): jesli dane nie spełniają żadnego warunku program nie zwaraca nic
-/// 
-/// UWAGA:
-/// Twój program zawsze powinien zwracać TYLKO EDEN KOMUNIKAT lub w przypadu (4) nic!
+﻿string name = "Agnieszka";
+bool male = true;
+int age = 20  ;
+
+if ((male == false) && (age < 30))
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+} 
+
+if (name == "Ewa" && (age == 30))
+{
+    Console.WriteLine(name + ", lat " + age);
+}
+
+if (male &&  (age < 18))
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
