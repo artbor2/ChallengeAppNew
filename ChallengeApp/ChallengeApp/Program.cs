afc5322eb3b86eef8f432266d1cb9b25111cabc7
Artur Borkowski <artbor2@gmail.com>
2024-02-11 08:30:39 +0100

    Zadanie Domowe Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,18 @@
-﻿var age = 99;
-string name = "Adam";
-
-if (age > 50)
-{
-    Console.WriteLine("MAM POWYZEJ  50 LAT");
-    if (name == "Adam")
-    {
-        Console.WriteLine("JESTEM ADAM");
-    }
-    else
-    {
-        Console.WriteLine("JESTEM KTOŚ INNY");
-    }
-}
-else 
-{
-    Console.WriteLine("40-50 latek ");
-}
+﻿/// Napisz w Program.cs kod, w którym:
+/// - zadeklarujesz zmienną z imieniem
+/// - zadeklarujesz zmienną z płcią (dobierz odpowiednią)
+/// - zadeklarujesz zmienną z wiekiem
+/// 
+/// A następnie napisz kilka instrukcji warunkowych,
+/// które przeanalizują te dane w następujący sposób:
+///  
+/// (1): jeśli mamy do czynienia z kobietą i ma poniżej 30 lat, to zwróć komunikat:
+///      "Kobieta poniżej 30 lat"
+/// (2): jeśli mamy do czynienia z osobą o imieniu Ewa i ma ona równe 30 lat,
+///      to zwróć komunikat: "Ewa, lat 30"
+/// (3): jeśli mamy do czynienia z mężczyzną i ma poniżej 18 lat zwróć komunikat:
+///      "Niepełnoletni mężczyzna"
+/// (4): jesli dane nie spełniają żadnego warunku program nie zwaraca nic
+/// 
+/// UWAGA:
+/// Twój program zawsze powinien zwracać TYLKO EDEN KOMUNIKAT lub w przypadu (4) nic!
