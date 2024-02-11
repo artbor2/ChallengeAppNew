a9f27c12b7f7e6cbf1813055a462de2c2483cbe2
Artur Borkowski <artbor2@gmail.com>
2024-02-11 10:30:44 +0100

    Listy, pętla foreach

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,32 +1,20 @@
-﻿int[] grades = new int[5];
-string[] dayOfWeeks = new string[7];
-dayOfWeeks[0] = "poniedziałek";
-dayOfWeeks[1] = "wtorek";
-dayOfWeeks[2] = "środa";
-dayOfWeeks[3] = "czwartek";
-dayOfWeeks[4] = "piątek";
-dayOfWeeks[5] = "sobota";
-dayOfWeeks[6] = "niedziela";
-Console.WriteLine(dayOfWeeks[2]);
+﻿List<string> daysOfWeeks = new List<string>();
+daysOfWeeks.Add("poniedziałek");
+daysOfWeeks.Add("wtorek");
+daysOfWeeks.Add("środa");
+daysOfWeeks.Add("czwartek");
+daysOfWeeks.Add("piątek");
+daysOfWeeks.Add("sobota");
+daysOfWeeks.Add("niedziela");
 
-string[] dayOfWeeks2 = { "poniedziałek", "wtorek", "środa",
-                         "czwartek", "piatek", "sobota", "niedziela"};
 
-Console.WriteLine(dayOfWeeks2[0]);
-
-for (int i = 0; i < 10; i = i + 1)
+for (int i = 0; i < daysOfWeeks.Count; i++)
 {
-    Console.Write(i + " ");
-}
-Console.WriteLine('\n');
-
-for (int i = 0;i < dayOfWeeks.Length; i++)
-{
-    Console.WriteLine(dayOfWeeks[i]);
+    Console.WriteLine(daysOfWeeks[i]);
 }
 Console.WriteLine();
 
-for (int i = 0; i < dayOfWeeks.Length; i = i + 2)
+foreach (string day in daysOfWeeks)
 {
-    Console.WriteLine(dayOfWeeks[i]);
+    Console.WriteLine(day);
 }
